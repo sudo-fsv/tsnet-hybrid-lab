62e8830e3c14a3923e548968b035431cc63597dd
sudo-fsv <felipeisnotabot@pm.me>
2025-12-17 22:15:42 -0800

    wrong variable name

--- github-secrets/main.tf
+++ github-secrets/main.tf
@@ -8,37 +8,37 @@ provider "github" {
 # provider `owner` + `repository` is used by resources below).
 
 resource "github_actions_secret" "aws_access_key" {
-  repository  = var.repository
+  repository  = var.github_repo
   secret_name = "AWS_ACCESS_KEY_ID"
   plaintext_value   = var.aws_access_key_id
 }
 
 resource "github_actions_secret" "aws_secret_key" {
-  repository  = var.repository
+  repository  = var.github_repo
   secret_name = "AWS_SECRET_ACCESS_KEY"
   plaintext_value   = var.aws_secret_access_key
 }
 
 resource "github_actions_secret" "aws_session_token" {
-  repository  = var.repository
+  repository  = var.github_repo
   secret_name = "AWS_SESSION_TOKEN"
   plaintext_value   = var.aws_session_token
 }
 
 resource "github_actions_secret" "tailscale_auth_key" {
-  repository  = var.repository
+  repository  = var.github_repo
   secret_name = "TAILSCALE_AUTH_KEY"
   plaintext_value   = var.tailscale_auth_key
 }
 
 resource "github_actions_secret" "tailscale_oauth_client_id" {
-  repository     = var.repository
+  repository     = var.github_repo
   secret_name    = "TAILSCALE_OAUTH_CLIENT_ID"
   plaintext_value = var.tailscale_oauth_client_id
 }
 
 resource "github_actions_secret" "tailscale_oauth_client_secret" {
-  repository     = var.repository
+  repository     = var.github_repo
   secret_name    = "TAILSCALE_OAUTH_CLIENT_SECRET"
   plaintext_value = var.tailscale_oauth_client_secret
 }
